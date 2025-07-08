b88fefb6ba0735cf84f3432eed2e651332eb64ae
James Reeves <jreeves@weavejester.com>
2025-07-09 00:18:28 +0100

    Add selector and connection accepting

--- src/teensyp/server.clj
+++ src/teensyp/server.clj
@@ -1,23 +1,48 @@
 (ns teensyp.server
   (:import [java.net InetSocketAddress]
-           [java.nio.channels ServerSocketChannel]))
+           [java.nio ByteBuffer]
+           [java.nio.channels Selector SelectionKey
+            ServerSocketChannel SocketChannel]))
 
 (defn- server-socket-channel ^ServerSocketChannel [port]
   (doto (ServerSocketChannel/open)
     (.configureBlocking false)
     (.bind (InetSocketAddress. port))))
 
+(defn- server-selector [^ServerSocketChannel server-ch]
+  (let [selector (Selector/open)]
+    (.register server-ch selector SelectionKey/OP_ACCEPT)
+    selector))
+
+(defn- iter-selection-keys [^Selector selector f]
+  (let [iter (-> selector .selectedKeys .iterator)]
+    (loop []
+      (when (.hasNext iter)
+        (f (.next iter))
+        (.remove iter)
+        (recur)))))
+
+(defn- handle-key [^SelectionKey key]
+  (when (.isValid key)
+    (when (.isAcceptable key)
+      (let [^SocketChannel ch (-> key .channel .accept)]
+        (.configureBlocking ch false)
+        (.write ch (ByteBuffer/wrap (.getBytes "hello\n")))
+        (.close ch)))))
+
+(defn- server-loop [^ServerSocketChannel server-ch ^Selector selector]
+  (loop []
+    (when (.isOpen server-ch)
+      (.select selector)
+      (iter-selection-keys selector handle-key)
+      (recur))))
+
 (defn- start-daemon-thread [^Runnable r]
   (doto (Thread. r) (.setDaemon true) (.start)))
 
-(defn- server-loop [^ServerSocketChannel server-ch]
-  (loop []
-    (when (.isOpen server-ch)
-      (Thread/sleep 1000)
-      (recur))))
-
 (defn start-server [{:keys [port]}]
   {:pre [(int? port)]}
-  (let [server-ch (server-socket-channel port)]
-    (start-daemon-thread #(server-loop server-ch))
+  (let [server-ch (server-socket-channel port)
+        selector  (server-selector server-ch)]
+    (start-daemon-thread #(server-loop server-ch selector))
     server-ch))
--- test/teensyp/server_test.clj
+++ test/teensyp/server_test.clj
@@ -1,7 +1,15 @@
 (ns teensyp.server-test
-  (:require [clojure.test :refer [deftest is]]
-            [teensyp.server :as tcp]))
+  (:require [clojure.java.io :as io]
+            [clojure.test :refer [deftest is]]
+            [teensyp.server :as tcp])
+  (:import [java.net Socket]))
 
-(deftest server-test
+(deftest server-close-test
   (with-open [server (tcp/start-server {:port 3456})]
     (is (instance? java.io.Closeable server))))
+
+(deftest server-read-test
+  (with-open [_server (tcp/start-server {:port 3457})]
+    (let [sock (Socket. "localhost" 3457)]
+      (with-open [reader (io/reader (.getInputStream sock))]
+        (is (= "hello" (.readLine reader)))))))
