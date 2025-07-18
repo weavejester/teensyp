a1d241068c219b7d446f194664a1d239a9042818
James Reeves <jreeves@weavejester.com>
2025-07-18 23:10:50 +0100

    Refactor AsyncByteBufferChannel

--- src/teensyp/channel.clj
+++ src/teensyp/channel.clj
@@ -4,7 +4,12 @@
            [java.nio.channels AsynchronousByteChannel Channel
             ClosedChannelException CompletionHandler
             ReadPendingException WritePendingException]
-           [java.util.concurrent Future FutureTask]))
+           [java.util.concurrent CompletableFuture Future]))
+
+(deftype FutureCompletionHandler [^CompletableFuture fut]
+  CompletionHandler
+  (completed [_ result _] (.complete fut result))
+  (failed [_ ex _] (.completeExceptionally fut ex)))
 
 (deftype AsyncByteBufferChannel
          [^:volatile-mutable ^ByteBuffer buffer
@@ -13,31 +18,18 @@
           ^:volatile-mutable open?]
   AsynchronousByteChannel
   (^Future read [this ^ByteBuffer buf]
-    (let [fut (FutureTask.
-               #(locking this
-                  (let [num (buf/copy buffer buf)]
-                    (when-not (.hasRemaining buffer)
-                      (set! (.buffer this) nil))
-                    (when (and pending-write (nil? buffer))
-                      (pending-write))
-                    num)))]
-      (locking this
-        (when-not open? (throw (ClosedChannelException.)))
-        (if (nil? buffer)
-          (if (nil? pending-read)
-            (set! pending-read #(.run fut))
-            (throw (ReadPendingException.)))
-          (.run fut))
-        fut)))
+    (let [fut (CompletableFuture.)]
+      (.read this buf nil (FutureCompletionHandler. fut))
+      fut))
   (^void read [this ^ByteBuffer buf att ^CompletionHandler handler]
     (letfn [(read-buffer []
               (locking this
                 (let [num (buf/copy buffer buf)]
                   (when-not (.hasRemaining buffer)
                     (set! (.buffer this) nil))
+                  (.completed handler num att)
                   (when (and pending-write (nil? buffer))
-                    (pending-write))
-                  (.completed handler num att))))]
+                    (pending-write)))))]
       (locking this
         (when-not open? (throw (ClosedChannelException.)))
         (if (nil? buffer)
@@ -46,32 +38,22 @@
             (throw (ReadPendingException.)))
           (read-buffer)))))
   (^Future write [this ^ByteBuffer buf]
-   (let [fut (FutureTask.
-              #(locking this
-                 (set! (.buffer this) buf)
-                 (when pending-read (pending-read))
-                 (.remaining buf)))]
-     (locking this
-       (when-not open? (throw (ClosedChannelException.)))
-       (if (nil? buffer)
-         (.run fut)
-         (if (nil? pending-write)
-           (set! pending-write #(.run fut))
-           (throw (WritePendingException.))))
-       fut)))
+    (let [fut (CompletableFuture.)]
+      (.write this buf nil (FutureCompletionHandler. fut))
+      fut))
   (^void write [this ^ByteBuffer buf att ^CompletionHandler handler]
-   (letfn [(write-buffer []
-             (locking this
-               (set! (.buffer this) buf)
-               (.completed handler (.remaining buf) att)
-               (when pending-read (pending-read))))]
-     (locking this
-       (when-not open? (throw (ClosedChannelException.)))
-       (if (nil? buffer)
-         (write-buffer)
-         (if (nil? pending-write)
-           (set! pending-write write-buffer)
-           (throw (WritePendingException.)))))))
+    (letfn [(write-buffer []
+              (locking this
+                (set! (.buffer this) buf)
+                (.completed handler (.remaining buf) att)
+                (when pending-read (pending-read))))]
+      (locking this
+        (when-not open? (throw (ClosedChannelException.)))
+        (if (nil? buffer)
+          (write-buffer)
+          (if (nil? pending-write)
+            (set! pending-write write-buffer)
+            (throw (WritePendingException.)))))))
   Channel
   (isOpen [_] open?)
   (close [_] (set! open? false)))
