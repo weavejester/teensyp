a2ea0ce9dd1cd123156995e671b5e7d4a33fd2bf
James Reeves <jreeves@weavejester.com>
2026-05-12 17:07:11 +0100

    Make IOutputStream extend java.io.Flushable

--- src/java/teensyp/IOutputStream.java
+++ src/java/teensyp/IOutputStream.java
@@ -1,8 +1,8 @@
 package teensyp;
 
 import java.io.Closeable;
+import java.io.Flushable;
 
-public interface IOutputStream extends Closeable {
+public interface IOutputStream extends Closeable, Flushable {
     public int write(byte[] b, int off, int len);
-    public void flush();
 }
